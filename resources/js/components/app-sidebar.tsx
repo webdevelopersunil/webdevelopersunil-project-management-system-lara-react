e9b49ecf8f8d7412d586fd61c86c2f9316bec00e
govind <govind@Mac.alphionsee.in>
2025-12-27 21:55:58 +0530

    Added menu on Sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,7 +13,7 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, Paperclip, Users } from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, Paperclip, Users, GitPullRequestCreateArrow } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -31,6 +31,11 @@ const mainNavItems: NavItem[] = [
         title: 'Users',
         href: '/users/',
         icon: Users,
+    },
+    {
+        title:  'Portal Requests',
+        href:   '/portal-requests/',
+        icon:   GitPullRequestCreateArrow,
     }
 ];
 
