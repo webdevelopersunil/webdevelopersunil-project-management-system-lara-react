3dc8599f17225bd1927efa6364c3587f5bcc70e9
sunil <he.kingsmen@gmail.com>
2026-04-09 16:50:57 +0530

    Describe requestor permissions

--- routes/portals.php
+++ routes/portals.php
@@ -9,15 +9,19 @@ Route::middleware('auth')->group(function () {
     
     Route::get('portals', [PortalController::class, 'index'])->name('portal.index');
 
-    Route::get('portals/create', [PortalController::class, 'create'])->name('portal.create');
+    Route::middleware('role:admin|project-manager|developer')->group(function () {
 
-    Route::post('portals/', [PortalController::class, 'store'])->name('portal.store');
+        Route::get('portals/create', [PortalController::class, 'create'])->name('portal.create');
+    
+        Route::post('portals/', [PortalController::class, 'store'])->name('portal.store');
+    
+        Route::delete('/portals/{portal}', [PortalController::class, 'destroy'])->name('portal.destroy');
+    
+        Route::get('/portals/{portal}/edit', [PortalController::class, 'edit'])->name('portal.edit');
 
-    Route::delete('/portals/{portal}', [PortalController::class, 'destroy'])->name('portal.destroy');
+        Route::put('/portals/{portal}', [PortalController::class, 'update'])->name('portal.update');
+    });
 
-    Route::get('/portals/{portal}/edit', [PortalController::class, 'edit'])->name('portal.edit');
-
-    Route::put('/portals/{portal}', [PortalController::class, 'update'])->name('portal.update');
     Route::get('/portals/{portal}', [PortalController::class, 'show'])->name('portal.show');
 
     Route::post('/portals/{portal}/collaborators', [PortalController::class, 'storeCollaborator'])->name('portal.collaborators.store');
